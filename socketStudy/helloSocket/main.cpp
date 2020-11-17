718c8ce4365142ee4cd82417729dee68c520c774
tianyahechy <tianyahechu2004@163.com>
2020-11-17 08:15:17 +0800

    启动winsock

--- socketStudy/helloSocket/main.cpp
+++ socketStudy/helloSocket/main.cpp
@@ -2,7 +2,13 @@
 #include <Windows.h>
 #include <WinSock2.h>
 
+#pragma comment(lib, "ws2_32.lib")
+
 int main()
 {
+	WORD ver = MAKEWORD(2, 2);
+	WSADATA dat;
+	WSAStartup(ver, &dat);
+	WSACleanup();
 	return 0;
 }
