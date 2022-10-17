b7027cae109d70fe5e6a45be5b0a9c30f660b7b9
tianyahechy <tianyahechu2004@163.com>
2022-10-17 08:27:22 +0800

    Add files via upload
    2.4简易服务器和客户端

---
+++ socketStudy/EasyTcpClient/Client.cpp
@@ -0,0 +1,56 @@
+﻿
+#define WIN32_LEAN_AND_MEAN
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <Windows.h>
+#include <WinSock2.h>
+#include <iostream>
+
+int main()
+{
+    WORD ver = MAKEWORD(2, 2);
+    WSADATA dat;
+    //启动windows socket2.x环境
+    WSAStartup(ver, &dat);
+
+    //用socketapi建立简易tcp客户端
+    //1,建立一个socket套接字
+    SOCKET _sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (INVALID_SOCKET == _sock)
+    {
+
+        std::cout << "建立套接字失败!!!" << std::endl;
+        return 0;
+    }
+    std::cout << "建立套接字成功!!!" << std::endl;
+    //2,连接服务器connect
+
+    sockaddr_in _sin = {};
+    _sin.sin_family = AF_INET;
+    _sin.sin_port = htons(4567);
+    _sin.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+    int ret = connect(_sock, (sockaddr*)&_sin, sizeof(sockaddr_in));
+    if (SOCKET_ERROR == ret)
+    {
+        std::cout << "连接服务器失败!!!" << std::endl;
+        return 0;
+    }
+    std::cout << "连接服务器成功!!!" << std::endl;
+    //3,接收服务器信息recv
+    char recvBuf[256] = {};
+    //接收的数据长度
+    int nLen = recv(_sock, recvBuf,256, 0);
+    if (nLen == 0)
+    {
+        std::cout << "接收服务器信息" << std::endl;
+        return 0;
+    }
+        std::cout << "接收到数据:" << recvBuf << std::endl;
+   
+    //4,closesocket 关闭套接字
+    closesocket(_sock);
+    //清除windows socket环境
+    WSACleanup();
+    getchar();
+    return 0;
+}
--- socketStudy/EasyTcpServer/Server.cpp
+++ socketStudy/EasyTcpServer/Server.cpp
@@ -6,6 +6,7 @@
 #include <WinSock2.h>
 #include <iostream>
 
+
 int main()
 {
     WORD ver = MAKEWORD(2, 2);
@@ -16,21 +17,28 @@ int main()
     //用socketapi建立简易tcp服务端
     //1,建立一个socket套接字
     SOCKET _sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (INVALID_SOCKET == _sock)
+    {
+
+        std::cout << "建立套接字失败!!!" << std::endl;
+        return 0;
+    }
+    std::cout << "建立套接字成功!!!" << std::endl;
     //2,bind绑定用于接受客户端连接的网络端口
     sockaddr_in _sin = {};
     _sin.sin_family = AF_INET;
     _sin.sin_port = htons(4567);
     _sin.sin_addr.S_un.S_addr = INADDR_ANY;
-    bool bBindSuccess = bind(_sock, (sockaddr*) &_sin, sizeof(_sin));
-    if (SOCKET_ERROR == bBindSuccess)
+    int ret = bind(_sock, (sockaddr*) &_sin, sizeof(_sin));
+    if (SOCKET_ERROR == ret)
     {
         std::cout << "绑定网络端口失败!!!" << std::endl;
         return 0;
     }
     std::cout << "绑定网络端口成功!!!" << std::endl;
     //3,listen监听网络端口
-    bool bListenSuccess = listen(_sock, 5);
-    if (SOCKET_ERROR == bListenSuccess)
+    ret = listen(_sock, 5);
+    if (SOCKET_ERROR == ret)
     {
         std::cout << "监听网络端口失败!!!" << std::endl;
         return 0;
